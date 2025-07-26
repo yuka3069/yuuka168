48c4e92e6fea4bbe276e3312e36d7b2b9e2c9d03
LAPTOP-0HPUCQM3\Yunyu <yunyuancheng@outlook.com>
2025-07-26 10:09:37 +0800

    fix comment type string -> number error

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
+
 function Comment() {
   return (
     <article className="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
       <footer className="flex justify-between items-center mb-2">
         <div className="flex items-center">
           <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
-            <img
+            <Image
               className="mr-2 w-6 h-6 rounded-full"
               src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
               alt="Michael Gough"
--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -30,7 +30,7 @@ export default function Comments({ slug }: { slug: string }) {
                   </label>
                   <textarea
                     id="comment"
-                    rows="6"
+                    rows={6}
                     className="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
                     placeholder="Write a comment..."
                     required
