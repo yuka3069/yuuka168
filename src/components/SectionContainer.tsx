780374f2bcb2b9be310f226db10ecfb84ccae351
yunyuancheng@outlook.com <yunyuancheng@outlook.com>
2026-06-05 19:31:58 +0800

    change the background

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -1,11 +1,13 @@
-import { ReactNode } from 'react'
+import { ReactNode } from "react";
 
 interface Props {
-  children: ReactNode
+  children: ReactNode;
 }
 
 export default function SectionContainer({ children }: Props) {
   return (
-    <section className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</section>
-  )
+    <section className="site-wrapper relative mx-auto max-w-2xl bg-white px-6 sm:px-8 xl:max-w-4xl dark:bg-gray-800">
+      {children}
+    </section>
+  );
 }
